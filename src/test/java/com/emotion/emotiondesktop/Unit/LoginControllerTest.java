9aa283c9a7728f11fa0aa0c91162067b2578a416
BarTracz <bartosztracz68@gmail.com>
2024-05-14 14:26:06 +0200

    Update LoginControllerTest.java

--- src/test/java/com/emotion/emotiondesktop/Unit/LoginControllerTest.java
+++ src/test/java/com/emotion/emotiondesktop/Unit/LoginControllerTest.java
@@ -32,7 +32,7 @@ public class LoginControllerTest {
     }
 
     @Test
-    void testUserLogInWhenFieldsAreEmpty() {
+    void testUserCannotLogInWhenFieldsAreEmpty() {
         TextField emailField = mock(TextField.class);
         when(emailField.getText()).thenReturn("");
 
@@ -47,7 +47,7 @@ public class LoginControllerTest {
     }
 
     @Test
-    void testUserLogInWhenInvalidEmailFormat() {
+    void testUserCannotLoginWithInvalidEmail() {
         TextField emailField = mock(TextField.class);
         PasswordField passwordField = mock(PasswordField.class);
         when(emailField.getText()).thenReturn("invalidemail");
